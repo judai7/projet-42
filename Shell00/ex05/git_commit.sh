7ecdc1720f4e24d932a80ed68f62487fe4b6e206
Charles <81988326+judai7@users.noreply.github.com>
2024-08-13 15:35:13 +0200

    Add files via upload

---
+++ Shell00/ex05/git_commit.sh
@@ -0,0 +1,5 @@
+
+git clone https://github.com/judai7/git-vogsphere.42paris.fr-vogsphere-intra-uuid-46b8b77f-8dcd-4404-a7a9-77b6fac70b10-5921927-crafiki
+ cd git-vogsphere.42paris.fr-vogsphere-intra-uuid-46b8b77f-8dcd-4404-a7a9-77b6fac70b10-5921927-crafiki
+git log --format='%H' -5
+
